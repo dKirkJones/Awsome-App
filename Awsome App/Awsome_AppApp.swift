ec3813a1b05d0df14d2408a9e922c338d85e6a05
Darrin K Jones <djmakesapps@gmail.com>
2024-12-18 10:45:06 -0600

    Initial Commit

---
+++ Awsome App/Awsome_AppApp.swift
@@ -0,0 +1,32 @@
+//
+//  Awsome_AppApp.swift
+//  Awsome App
+//
+//  Created by Darrin Jones on 12/18/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Awsome_AppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
