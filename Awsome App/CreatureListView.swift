0d3f74db88277f11186093ec556a6d474742eb2c
Darrin K Jones <djmakesapps@gmail.com>
2024-12-19 15:35:03 -0600

    Added creature count to bottom of screen

--- Awsome App/CreatureListView.swift
+++ Awsome App/CreatureListView.swift
@@ -13,17 +13,22 @@ struct CreatureListView: View {
 
     var body: some View {
         NavigationStack {
-            List(creatures.creaturesArray, id: \.self) { creature in
+            List(0..<creatures.creaturesArray.count, id: \.self) { index in
                 NavigationLink {
-                   DetailView(creature: creature)
+                   DetailView(creature: creatures.creaturesArray[index])
                 } label: {
-                    Text(creature.name.capitalized)
+                    Text("\(index + 1)")
+                    Text(creatures.creaturesArray[index].name.capitalized)
                         .font(.title2)
                 }
-                
             }
             .listStyle(.plain)
             .navigationTitle("Pokemon")
+            .toolbar {
+                ToolbarItem(placement: .status) {
+                    Text("Creature Count \(creatures.creaturesArray.count) of \(creatures.count)")
+               }
+            }
         }
         .task {
             await creatures.getData()
